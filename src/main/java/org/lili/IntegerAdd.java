92f0939d23b529a5fb4ca341239d3cea8e3689db
lili <lili@chainup.com>
2020-03-21 20:12:31 +0800

    tdd sample and book

---
+++ src/main/java/org/lili/IntegerAdd.java
@@ -0,0 +1,9 @@
+package org.lili;
+
+/**
+ * @author lili
+ * @date 2020/1/4 1:00
+ * @description
+ */
+public class IntegerAdd {
+}
---
+++ src/test/java/org/lili/IntegerAddTest.java
@@ -0,0 +1,22 @@
+package org.lili;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lili
+ * @date 2020/1/4 1:01
+ * @description
+ */
+public class IntegerAddTest {
+    @Test
+    public void testAdd() {
+        Integer a = new Integer(1);
+        Integer b = a + new Integer(5);
+        assertEquals(1, a.intValue());
+        assertEquals(6, b.intValue());
+        assertTrue(new Integer(100).equals(new Integer(100)));
+    }
+
+}
